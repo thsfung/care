07b6df900a134767c2da0d5b113cb9302767ec37
Auuster <aaron.ks.au.91@gmail.com>
2017-08-01 20:44:07 -0400

    invoice_id on visits are now optional

--- care/app/models/visit.rb
+++ care/app/models/visit.rb
@@ -1,7 +1,7 @@
 class Visit < ApplicationRecord
 
 	belongs_to :patient, :foreign_key => "patient_id"
-	belongs_to :invoice, :foreign_key => "invoice_id"
+	belongs_to :invoice, :foreign_key => "invoice_id", optional: true
 	has_many :staff
 	has_one :service
 
